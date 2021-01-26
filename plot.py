54a01b99109a988727fa76caf959faaec1dc1b74
Piotr Jurkiewicz <piotr.jerzy.jurkiewicz@gmail.com>
2021-01-26 17:13:31 +0100

    Improve plotting script

--- plot.py
+++ plot.py
@@ -36,17 +36,17 @@ def subplots(n, **k):
     matplotlib.rcParams['figure.subplot.top'] = 1.0 - 0.35 / k['figsize'][1]
     matplotlib.rcParams['figure.subplot.wspace'] = 0.35
     matplotlib.rcParams['figure.subplot.hspace'] = 0.3
-    matplotlib.rcParams['axes.labelsize'] = 8
-    matplotlib.rcParams['axes.titlesize'] = 8
+    matplotlib.rcParams['axes.labelsize'] = 10
+    matplotlib.rcParams['axes.titlesize'] = 10
     matplotlib.rcParams['axes.titleweight'] = 'medium'
-    matplotlib.rcParams['xtick.labelsize'] = 8
-    matplotlib.rcParams['ytick.labelsize'] = 8
+    matplotlib.rcParams['xtick.labelsize'] = 10
+    matplotlib.rcParams['ytick.labelsize'] = 10
     matplotlib.rcParams['xtick.major.width'] = 0.5
     matplotlib.rcParams['xtick.minor.width'] = 0.25
     matplotlib.rcParams['ytick.major.width'] = 0.5
     matplotlib.rcParams['ytick.minor.width'] = 0.25
     matplotlib.rcParams['axes.xmargin'] = 0.0
-    matplotlib.rcParams['legend.fontsize'] = 8
+    matplotlib.rcParams['legend.fontsize'] = 10
     matplotlib.rcParams['legend.frameon'] = False
     if k.pop('latex', False):
         matplotlib.rcParams['text.usetex'] = True
@@ -106,30 +106,33 @@ def load_data(path):
         d['elapsed'] = d['elapsed'].astype(float) / 1000000000
         d['usage'] = d['cum_util'] / (d['edges'].astype(np.uint64) * d['units'])
         d['usage'] = d['usage'].round(2)
-        df[tuple(hdf.stem.split('_'))] = d
+        runtime, algorithm = tuple(hdf.stem.split('_'))
+        if runtime == 'python3':
+            runtime = 'cpython'
+        df[runtime, algorithm] = d
 
     return df
 
 def plot_hist(ss, name, relative=False):
     fs = (13, 2.5)
-    gs = {'wspace': 0.1, 'left': 0.045, 'right': 0.94, 'top': 0.98}
+    gs = {'wspace': 0.1, 'left': 0.05, 'right': 0.95, 'top': 0.98, 'bottom': 0.165}
     bbox = dict(facecolor='w', edgecolor='none', boxstyle='square,pad=0.2')
     with subplots(0, ncols=len(ss), nrows=1, figsize=fs, gridspec_kw=gs, sharey='row', latex=True) as (fig, axes):
         axes = iter(axes.flatten())
-        for s in ss:
+        for k, v in ss.items():
             ax = next(axes)
-            s = s.sort_values()
-            s.index = np.linspace(0.0, 1.0, num=len(s), endpoint=False)
+            v = v['elapsed'].sort_values()
+            v.index = np.linspace(0.0, 1.0, num=len(v), endpoint=False)
 
-            s.plot(ax=ax, style='k')
+            v.plot(ax=ax, style='k', label=k)
             ax.set_yscale('log')
             x_start, x_end = [0.0, 1.0]
-            y_start, y_end = [s.iloc[0] * 4, s.iloc[-1] / 12]
+            y_start, y_end = [v.iloc[0] * 4, v.iloc[-1] / 12]
 
             if relative:
-                prog_position = np.searchsorted(s, 1.0, side='left')
-                y = s.iloc[prog_position]
-                x = s.index[prog_position]
+                prog_position = np.searchsorted(v, 1.0, side='left')
+                y = v.iloc[prog_position]
+                x = v.index[prog_position]
                 ax.hlines(y=y, xmin=x_start, xmax=x, color='r', linestyle='--')
                 ax.vlines(x=x, ymin=y_start, ymax=y, color='r', linestyle='--')
                 print(x, y)
@@ -139,16 +142,16 @@ def plot_hist(ss, name, relative=False):
             else:
                 ax.set_ylabel('Time [seconds]')
 
-            y = s.median()
-            x = s.index[np.searchsorted(s, y, side='left')]
+            y = v.median()
+            x = v.index[np.searchsorted(v, y, side='left')]
             ax.hlines(y=y, xmin=x_start, xmax=x, color='b', linestyle='--')
             ax.vlines(x=x, ymin=y_start, ymax=y, color='b', linestyle='--')
             ax.text(x_start + 0.03, y, f'{y:0.3f} (median)', ha='left', va='center', fontsize=8, color='b', bbox=bbox)
             ax.text(x, y_start * 2, f'{x:0.3f}', ha='center', va='bottom', rotation='vertical', fontsize=8, color='b', bbox=bbox)
             print(x, y)
 
-            y = s.mean()
-            x = s.index[np.searchsorted(s, y, side='left')]
+            y = v.mean()
+            x = v.index[np.searchsorted(v, y, side='left')]
             ax.hlines(y=y, xmin=x_start, xmax=x, color='g', linestyle='--')
             ax.vlines(x=x, ymin=y_start, ymax=y, color='g', linestyle='--')
             ax.text(x_start + 0.03, y, f'{y:0.3f} (mean)', ha='left', va='center', fontsize=8, color='g', bbox=bbox)
@@ -160,6 +163,7 @@ def plot_hist(ss, name, relative=False):
 
             ax.set_xlabel('Fraction of calls')
             ax.yaxis.set_tick_params(labelleft=True)
+            ax.legend()
 
         # fig.savefig(f'{name}_cdf.png', metadata=PDF_NONE)
         fig.savefig(f'{name}_cdf.pdf', metadata=PDF_NONE)
@@ -191,17 +195,19 @@ def plot_fit(ax, z, fc):
 
 def plot_groups(s, name, relative=False):
     fs = (13, 2.5)
-    gs = {'wspace': 0.14, 'left': 0.045, 'right': 0.94, 'top': 0.98}
+    gs = {'wspace': 0.14, 'left': 0.05, 'right': 0.95, 'top': 0.97, 'bottom': 0.165}
+    cl = {'cpython': 'C2', 'pypy3': 'C3'}
     pathlib.Path(name).parent.mkdir(parents=True, exist_ok=True)
     with subplots(0, ncols=3, nrows=1, figsize=fs, gridspec_kw=gs, latex=True) as (fig, axes):
         axes = iter(axes.flatten())
         ax = next(axes)
-        z = s.groupby('nodes').mean()['elapsed']
-        z.plot(ax=ax, style='k.-', ms=4)
+        for k, v in s.items():
+            z = v.groupby('nodes').mean()['elapsed']
+            z.plot(ax=ax, style=f'{cl[k] if relative else "k"}.-', ms=4, y='elapsed', label=k)
         ax.margins(x=0.05)
         ax.autoscale(tight=False)
         ax.yaxis.set_major_formatter(StrMethodFormatter('{x:.3f}'))
-        ax.set_xticks(z.index)
+        ax.set_xticks(z.index[z.index / 25 % 2 == 0])
         ax.set_ylabel('Time [relative to filtered]' if relative else 'Time [seconds]')
         ax.set_xlabel('Number of nodes')
         if not relative:
@@ -213,8 +219,9 @@ def plot_groups(s, name, relative=False):
         ax.legend()
 
         ax = next(axes)
-        z = s.groupby('units').mean()['elapsed']
-        z.plot(ax=ax, style='k.-', ms=4)
+        for k, v in s.items():
+            z = v.groupby('units').mean()['elapsed']
+            z.plot(ax=ax, style=f'{cl[k] if relative else "k"}.-', ms=4, y='elapsed', label=k)
         ax.margins(x=0.05)
         ax.autoscale(tight=False)
         ax.yaxis.set_major_formatter(StrMethodFormatter('{x:.3f}'))
@@ -230,8 +237,9 @@ def plot_groups(s, name, relative=False):
         ax.legend()
 
         ax = next(axes)
-        z = s[s['usage'] <= 0.6].groupby('usage').mean()['elapsed']
-        z.plot(ax=ax, style='k.-', ms=4)
+        for k, v in s.items():
+            z = v[v['usage'] <= 0.6].groupby('usage').mean()['elapsed']
+            z.plot(ax=ax, style=f'{cl[k] if relative else "k"}.-', ms=4, y='elapsed', label=k)
         ax.margins(x=0.05)
         ax.autoscale(tight=False)
         ax.yaxis.set_major_formatter(StrMethodFormatter('{x:.3f}'))
@@ -264,7 +272,7 @@ def main():
 
     pd.set_option('float_format', '{:.6f}'.format)
 
-    for result in ['all']:
+    for result in ['all', 'good', 'bad']:
         if result == 'all':
             dd = df
         elif result == 'good':
@@ -275,17 +283,17 @@ def main():
             raise ValueError
 
         for k, v in dd.items():
-            plot_groups(v, result + '/' + '_'.join(k))
-            plot_hist([v['elapsed']], result + '/' + '_'.join(k))
+            plot_groups({k: v}, result + '/' + '_'.join(k))
+            plot_hist({k: v}, result + '/' + '_'.join(k))
 
-        ss = []
-        for interpreter in ['python3', 'pypy3']:
+        rel = {}
+        for interpreter in ['cpython', 'pypy3']:
             d = dd[interpreter, 'generic'].copy()
             d['elapsed'] /= dd[interpreter, 'filtered']['elapsed']
-            plot_groups(d, result + '/' + f'{interpreter}_relative_to_filtered', relative=True)
-            ss.append(d['elapsed'])
+            rel[interpreter] = d
 
-        plot_hist(ss, result + '/' + f'relative_to_filtered', relative=True)
+        plot_groups(rel, result + '/' + f'relative_to_filtered', relative=True)
+        plot_hist(rel, result + '/' + f'relative_to_filtered', relative=True)
 
 
 if __name__ == '__main__':
